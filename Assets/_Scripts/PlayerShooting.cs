869b3d3f3c21f8e706d57c285b0056ace4156948
inatchee <300767752@cencol.ca>
2016-11-21 11:20:39 -0500

    second clean version

---
+++ Assets/_Scripts/PlayerShooting.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShooting : MonoBehaviour {
+
+	// PUBLIC VARIABLES FOR TESTING
+	public Transform FlashPoint;
+	public GameObject MuzzleFlash;
+	public GameObject Explosion;
+	public GameObject BulletImpact;
+	public AudioSource RifleShotSound;
+	public Transform PlayerCam;
+
+	// PRIVATE VARIABLES
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame (for Physics)
+	void FixedUpdate () {
+		if (Input.GetButtonDown ("Fire1")) {
+			// show the MuzzleFlash at the FlashPoint without any rotation
+			Instantiate (this.MuzzleFlash, this.FlashPoint.position, Quaternion.identity);
+
+			// need a variable to hold the location of our Raycast Hit
+			RaycastHit hit;
+
+			// if raycast hits an object then do something...
+			if (Physics.Raycast (this.PlayerCam.position, this.PlayerCam.forward, out hit)) {
+
+				if (hit.transform.gameObject.CompareTag ("Alien")) {
+					Instantiate (this.Explosion, hit.point, Quaternion.identity);
+					Destroy (hit.transform.gameObject);
+				} else {
+					Instantiate (this.BulletImpact, hit.point, Quaternion.identity);
+				}
+
+
+			}
+
+			// Play Rifle Sound
+			this.RifleShotSound.Play();
+		}
+	}
+}
